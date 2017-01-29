9830d4c43ee0a53c996ea43f59f1dd3cfcf224e6
MordRot <mord.rot@gmail.com>
2017-01-29 15:21:03 +0200

    בדיקה שנתונים עוברים דרל ה CP בין אפליקציות שונות, זה עובד

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300_travel_finder/Model/Backend/TravelsReceiver.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300_travel_finder/Model/Backend/TravelsReceiver.java
@@ -3,20 +3,36 @@ package com.android.mor_arye.android5777_8159_8300_travel_finder.Model.Backend;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
 public class TravelsReceiver extends BroadcastReceiver {
+    public static final String TravelApp_TAG = "TravelApp";
+    private Context c;
     public TravelsReceiver() {
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         if (intent.getAction().equalsIgnoreCase("com.android.mor_arye.android5777_8159_8300.newUpdates")) {
             char type = intent.getExtras().getChar("table");
             if (type == 'b'){
-                Log.d("checkBR", "business");
-//            Toast.makeText(this, "sent update broadcast", Toast.LENGTH_SHORT).show();
+                Log.d(TravelApp_TAG, "business");
+
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        Uri uriOfAllUsers = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/businesses");
+                        Cursor result = context.getContentResolver().query(uriOfAllUsers,null,null,null,null);
+                        result.moveToFirst();
+                        String str = result.getString(result.getColumnIndex("nameBusiness"));
+                        Log.d(TravelApp_TAG, str);
+                        return null;
+                    }
+                }.execute();
+
             }
             else if (type == 'r')
                 Log.d("checkBR", "recreations");

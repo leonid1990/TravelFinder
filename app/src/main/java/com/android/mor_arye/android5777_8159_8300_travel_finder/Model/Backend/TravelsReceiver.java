26b9eddcb81c340aa04ed6eb23f908e3e193d7f9
Arye <leonid1990@gmail.com>
2017-02-11 22:57:05 +0200

    BR Works. Connection to the CP shouldn't be performed in onReceive, even by AsyncTask: http://stackoverflow.com/questions/21743800/call-asynctask-from-broadcast-receiver-android

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300_travel_finder/Model/Backend/TravelsReceiver.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300_travel_finder/Model/Backend/TravelsReceiver.java
@@ -14,94 +14,85 @@ import android.widget.Toast;
 public class TravelsReceiver extends BroadcastReceiver {
     public static final String BR_TAG = "TravelsReceiver";
     private static IDSManager DSManager = ManagerFactory.getDS();
-/*
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Action: " + intent.getAction() + "\n");
-        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
-        String log = sb.toString();
-        Log.d(BR_TAG, log);
-        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
-    }*/
+
+    /*
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Action: " + intent.getAction() + "\n");
+            sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+            String log = sb.toString();
+            Log.d(BR_TAG, log);
+            Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+        }
+        */
     public TravelsReceiver() {
     }
-Context c;
+
+    Context c;
+
     @Override
     public void onReceive(final Context context, Intent intent) {
         c = context;
         if (intent.getAction().equalsIgnoreCase("com.android.mor_arye.android5777_8159_8300.newUpdates")) {
 
             char type = intent.getExtras().getChar("table");
-            if (type == 'b'){
-                Log.d(BR_TAG, "business");
-
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        Uri uriOfAllBusinesses = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/businesses");
-                        Cursor result = context.getContentResolver().query(uriOfAllBusinesses,null,null,null,null);
-                        final ContentValues business = new ContentValues();
-                        if (result.moveToFirst())
-                        {
-                            do
-                            {
-                                DatabaseUtils.cursorRowToContentValues (result, business);
-                                DSManager.insertBusiness(business);
-                            }
-                            while(result.moveToNext());
+            if (type == 'b') {
+                Log.d(BR_TAG, "businesses");
+                Uri uriOfAllBusinesses = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/businesses");
+                try {
+                    Cursor result = context.getContentResolver().query(uriOfAllBusinesses, null, null, null, null);
+                    final ContentValues business = new ContentValues();
+                    if (result.moveToFirst()) {
+                        do {
+                            DatabaseUtils.cursorRowToContentValues(result, business);
+                            DSManager.insertBusiness(business);
                         }
-                        return null;
+                        while (result.moveToNext());
                     }
-                }.execute();
-            }
-            else if (type == 'r') {
+                } catch (Exception ex) {
+                    Log.d(BR_TAG, ex.getMessage());
+                }
+            } else if (type == 'r') {
                 Log.d(BR_TAG, "recreations");
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        Uri uriOfAllRecreations = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/recreations");
-                        Cursor result = context.getContentResolver().query(uriOfAllRecreations,null,null,null,null);
-                        final ContentValues recreation = new ContentValues();
-                        if (result.moveToFirst())
-                        {
-                            do
-                            {
-                                DatabaseUtils.cursorRowToContentValues (result, recreation);
-                                DSManager.insertBusiness(recreation);
-                            }
-                            while(result.moveToNext());
+                try {
+                    Uri uriOfAllRecreations = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/recreations");
+                    Cursor result = context.getContentResolver().query(uriOfAllRecreations, null, null, null, null);
+                    final ContentValues recreation = new ContentValues();
+                    if (result.moveToFirst()) {
+                        do {
+                            DatabaseUtils.cursorRowToContentValues(result, recreation);
+                            DSManager.insertRecreation(recreation);
                         }
-                        return null;
+                        while (result.moveToNext());
                     }
-                }.execute();
-            }
-            else
+                } catch (Exception ex) {
+                    Log.d(BR_TAG, ex.getMessage());
+                }
+            } else
                 Log.d(BR_TAG, "error");
+
+            getAllBusinesses();
         }
     }
-   /*
+
     private void getAllBusinesses() {
-
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-
-                Uri uriOfAllBusinesses = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/businesses");
-                Cursor result = c.getContentResolver().query(uriOfAllBusinesses,null,null,null,null);
-                String str;
-                if (result.moveToFirst())
-                {
-                    do
-                    {
-                        str = result.getString(result.getColumnIndex("nameBusiness"));
-                        Log.d(BR_TAG, str);
-                    }
-                    while(result.moveToNext());
+        try
+        {
+            Uri uriOfAllBusinesses = Uri.parse("content://com.android.mor_arye.android5777_8159_8300/businesses");
+            Cursor result = c.getContentResolver().query(uriOfAllBusinesses, null, null, null, null);
+            String str;
+            if (result.moveToFirst()) {
+                do {
+                    str = result.getString(result.getColumnIndex("nameBusiness"));
+                    Log.d(BR_TAG, str);
                 }
-                return null;
+                while (result.moveToNext());
             }
-        }.execute();
+        }
+        catch (Exception ex)
+        {
+            Log.d(BR_TAG, ex.getMessage());
+        }
     }
-    */
 }
